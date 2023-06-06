fede46892f8981f8836000f6babe0fb4efbdf6e3
mai <fujisawamai21@gmail.com>
2023-06-06 17:02:45 +0900

    ControllerにGet, Post Patch, Deleteを実装した。

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TaskDto } from './todo.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -9,5 +18,38 @@ export class TodoController {
   async getList() {
     const result = await this.prisma.task.findMany();
     return [...result];
+  }
+
+  @Post('')
+  async add(@Body() task: TaskDto) {
+    const result = await this.prisma.task.create({
+      data: {
+        title: task.title,
+      },
+    });
+    return result;
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() task: TaskDto) {
+    const result = await this.prisma.task.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title: task.title,
+      },
+    });
+    return result;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const result = await this.prisma.task.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return result;
   }
 }
